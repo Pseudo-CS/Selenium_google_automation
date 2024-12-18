c6b27641ea2afb41621d1d218260c94844ed5097
Pseudo <73516975+Pseudo-CS@users.noreply.github.com>
2024-12-18 22:27:48 +0530

    Update send_assignment.py

--- selenium_proj/selenium_proj/send_assignment.py
+++ selenium_proj/selenium_proj/send_assignment.py
@@ -2,7 +2,7 @@ from django.core.mail import EmailMessage
 
 def send_assignment_email():
     subject = "Python (Selenium) Assignment - Saandeep CS"
-    body = "git repo link: https://github.com/saandeepcs/selenium_proj.git"
+    body = "git repo link: https://github.com/Pseudo-CS/Selenium_google_automation.git"
     from_email = "saandeepcs010@gmail.com" 
     to_email = ["tech@themedius.ai"]
     cc_email = ["hr@themedius.ai"]
